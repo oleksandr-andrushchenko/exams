e1d0743d7a4fc94a5cb1b49291aa78be3dec4631
Oleksandr Andrushchenko <oleksandr.andrushchenko1988@gmail.com>
2026-08-14 16:07:32 -0500

    chore: stop tracking generated dist artifacts

--- web-lambda/dist/local.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server");
-const port = Number(process.env.PORT || 3000);
-server_1.app.listen(port, '0.0.0.0', () => console.log(`ExamMe SSR listening on ${port}`));
